889e4c1cd25eb8fd85e53e08e8e7d9156a57c943
Max Burger <aburger@twitter.com>
2016-06-23 13:00:06 -0700

    trying something new

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/basic', function(req, res, next){
 });
 
 router.get('/loveislove', function(req, res, next){
-	res.render('loveisLove', {title: '#LoveisLove' });
+	res.render('loveisLove.jade', {title: '#LoveisLove' });
 });
 
 module.exports = router;
